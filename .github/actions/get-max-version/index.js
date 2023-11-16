d80a2c4bcda70a787ffd3a5d4ef37555914a1fbd
Michal Pasinski <pasinski@gmail.com>
2023-11-16 18:11:33 +0100

    push changelog

--- .github/actions/get-max-version/index.js
+++ .github/actions/get-max-version/index.js
@@ -1,7 +1,5 @@
 const core = require('@actions/core');
 const parseChangelog = require('changelog-parser');
-const _ = require("underscore");
-
 
 try {
     // `who-to-greet` input defined in action metadata file
@@ -9,11 +7,8 @@ try {
     console.log(`Path to changelog ${pathToChangelog}`);
     parseChangelog(pathToChangelog)
         .then( result => {
-            const versions = result.versions;
-            const versionsNoNull = versions.filter( version => version.version !== null).map( version => version.version);
-            const maxVersion = _.max( versionsNoNull );
-            console.log(`Versions ${JSON.stringify(versions)}`);
-            console.log(`Versions no null ${JSON.stringify(versionsNoNull)}`);
+            const maxVersion = result.versions.filter( version => version.version !== null).map( version => version.version)
+                .sort( (a,b) => a.localeCompare(b))[0];
             console.log(`max version ${maxVersion}`);
             core.setOutput("max-version", maxVersion );
         } )
