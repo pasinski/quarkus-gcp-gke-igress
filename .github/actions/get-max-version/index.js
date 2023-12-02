cb027780a37dc7ea3b94e976b88b17e4037e5b43
Michal Pasinski <pasinski@gmail.com>
2023-12-02 17:23:18 +0100

    Revert "push changelog"
    This reverts commit fd281947d668e2d804d65c22ce654c49097ba201.

--- .github/actions/get-max-version/index.js
+++ .github/actions/get-max-version/index.js
@@ -11,8 +11,7 @@ try {
         .then( result => {
             const versions = result.versions;
             const maxVersion = _.max( versions.filter( version => version.version !== null).map( version => version.version) );
-            console.log(`Versions ${JSON.stringify(versions)}`);
-            console.log(`Versions ${maxVersion}`);
+            console.log(`Versions ${versions}`);
             core.setOutput("max-version", maxVersion );
         } )
         .catch(error => core.setFailed(error.message))
